52b18e11e2aed92f96f371b8c301c70814e9e037
DataScienceGB <gabriel.banuelos@infomedia.com.mx>
2015-03-09 13:50:59 -0600

    Pollute final

---
+++ pollutantmean.R
@@ -0,0 +1,37 @@
+pollutantmean <- function (directory, pollutant,id=1:332) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+
+        ## 'pollutant' is a character vector of length 1 indicating
+        ## the name of the pollutant for which we will calculate the
+        ## mean; either "sulfate" or "nitrate".
+
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+
+        ## Return the mean of the pollutant across all monitors list
+        ## in the 'id' vector (ignoring NA values)
+
+        ## Recibe los argumentos como los pide el ejercicio
+        ## y los renombra a los usados durante la programacion
+        ## 20150309 DataScienceGB
+
+fpath=directory
+element=pollutant
+lodata=id
+ 
+ldata=list.files(fpath) ## Lista todos los archivos de la ruta
+flag=1                  ## permite crear un DF vacio y luego concatenar
+  for (i in lodata) {   ## para todo el vector solicitado
+    ndata=paste0(fpath,ldata[i])
+    df1<-read.csv(ndata)
+    if (flag==1) {
+        dft=df1
+    } 
+    else {
+        flag=0
+        dft<-rbind(dft,df1)
+    }
+  }
+  mean(dft[,element],na.rm=TRUE)  ## obtiene la media de la columna indicada
+} ##END
